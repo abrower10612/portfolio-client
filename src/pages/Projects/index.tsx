e27db6d127790c070d7c301b75dc851007feb653
Andrew Brower <abrower10612@gmail.com>
2024-01-19 15:58:40 -0700

    add scroll lock to Projects page

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -1,7 +1,10 @@
 import Header from '../../components/Shared/Header';
+import useScrollLock from '../../hooks/useScrollLock';
 import styles from './styles.module.scss';
 
 const Projects = () => {
+  useScrollLock();
+
   return (
     <div className={styles.container}>
       <Header title='Projects' gradient />
